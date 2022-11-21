e877e87239905a5e833c72aac44fcdd09a8dc4fa
Naju.Oliveira <a.julia.barbosa@outlook.com>
2022-11-20 22:07:12 -0300

    Terminando vídeo aula
    Ultima parte com o acompanhamento dos vídeos aulas do crie seus jogos

--- Flappy Christmas/Assets/Script/GameController.cs
+++ Flappy Christmas/Assets/Script/GameController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-
+    public int score;
+    public Text scoreText;
     private void Start()
     {
         Time.timeScale = 1;
